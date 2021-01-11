00aebe985ff38ee20f6c5277b74f157720eb4f3f
Anselm Baur <anselm.baur@desy.de>
2021-01-11 18:24:08 +0100

    forget update setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="b2style-anselm-baur", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Anselm Baur",
     author_email="anselm.baur@desy.de",
     description="style package for Belle II plots",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/anselm-baur/b2style",
+    url="https://stash.desy.de/users/abaur/repos/b2style/browse",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
